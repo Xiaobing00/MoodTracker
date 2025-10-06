e29b241bc39de700f1ef65176a44cfd9120bf399
Xiaobing00 <tailsnappy@gmail.com>
2025-10-05 23:05:04 -0400

    update main

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-print("Hello! Welcome to my mood tracker test.")
-print("Which day is today?")
-print("Monday, Tuesday, Thursday, Friday or Saturday?")
+print("Hello, welcome to my mood tracker test.")
+print("Please tell me, which day is today?")
+print("Monday, Tuesday, Wednesday, Thursday, Friday or Saturday.")
 
+day = input("Type your day:")
+print("Today is", day)
+
+mood = input("From 1 to 10, how is your mood today?")
+print("You feel", mood )
+
+note = input("Please write one short note about your day:")
+print("You wrote:", note)
+
+print("Thank you. Your note is", note)
+print("Bye.")
